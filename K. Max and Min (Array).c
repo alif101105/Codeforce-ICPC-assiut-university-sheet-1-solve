8f17cb23ea6fb56cccb9c127bc2c4ddeff8ab296
Maksudul Azad Alif <maksudulazadalif@gmail.com>
2026-07-10 12:35:53 +0600

    Add files via upload

---
+++ K. Max and Min (Array).c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+ {
+     int i, n=4, arr[10], min, max;
+     for(i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+     min=max= arr[0];
+     for(i=1;i<n;i++)
+     {
+        if(arr[i]>max)
+        {
+            max= arr[i];
+        }
+        if(arr[i]<min)
+        {
+            min= arr[i];
+        }
+     }
+     printf("%d %d", min, max);
+     return 0;
+ }
+
