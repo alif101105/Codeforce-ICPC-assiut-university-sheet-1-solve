b1cc22db8d7341850a098120d5a82edff6b9e050
Maksudul Azad Alif <maksudulazadalif@gmail.com>
2026-07-10 12:43:33 +0600

    Update A. Say Hello With C++.cpp

--- A. Say Hello With C++.cpp
+++ A. Say Hello With C++.cpp
@@ -1,8 +1,9 @@
-#include<stdio.h>
- int main()
- {
-     char ar[100];
-     scanf("%s",&ar);
-     printf("Hello, %s", ar);
-     return 0;
- }
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string S;
+    cin>>S;
+    cout<<"Hello, "<<S;
+
+}
