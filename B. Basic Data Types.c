f66af7c4880cfd80f23efd2579a5aba206bac4ce
Maksudul Azad Alif <maksudulazadalif@gmail.com>
2026-07-12 18:09:58 +0600

    Update B. Basic Data Types.c

--- B. Basic Data Types.c
+++ B. Basic Data Types.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int a;
-    long long b;
-    char ch;
-    float f;
-    double d;
-    scanf("%d %lld %c %f %lf", &a, &b, &ch, &f, &d);
-    printf("%d\n", a);
-    printf("%lld\n", b);
-    printf("%c\n", ch);
-    printf("%.2f\n", f);
-    printf("%.1lf", d);
+    long long x,y,sum,mult,sub;
+    scanf("%lld %lld",&x,&y);
+    sum=x+y;
+    mult=x*y;
+    sub=x-y;
+    printf("%lld + %lld = %lld\n",x,y,sum);
+    printf("%lld * %lld = %lld\n",x,y,mult);
+    printf("%lld - %lld = %lld",x,y,sub);
     return 0;
 }
