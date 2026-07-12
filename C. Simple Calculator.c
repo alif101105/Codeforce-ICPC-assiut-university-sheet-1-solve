b9c24e158cd2442ce2e59b21a5f98736346a2e5f
Maksudul Azad Alif <maksudulazadalif@gmail.com>
2026-07-12 17:54:51 +0600

    Add files via upload

---
+++ C. Simple Calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x=5, y=10, sum, mult, sub;
+    sum= x+y;
+    mult= x*y;
+    sub= x-y;
+    printf("5 + 10 = %d\n", sum);
+    printf("5 * 10 = %d\n", mult);
+    printf("5 - 10 = %d", sub);
+    return 0;
+}
