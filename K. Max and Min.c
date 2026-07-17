309d4f621a7424d2d1e39a319a216cff6ed2c122
Maksudul Azad Alif <maksudulazadalif@gmail.com>
2026-07-17 10:27:02 +0600

    Update K. Max and Min.c

--- K. Max and Min.c
+++ K. Max and Min.c
@@ -23,3 +23,38 @@
      printf("%d %d", min, max);
      return 0;
  }
+
+
+
+
+
+
+
+
+
+
+
+// Array diye kora hoise..
+#include<stdio.h>
+int main()
+ {
+     int i, n=3, arr[5], min, max;
+     for(i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+     min=max= arr[0];
+     for(i=1;i<n;i++)
+     {
+        if(arr[i]>max)
+        {
+            max= arr[i];
+        }
+        if(arr[i]<min)
+        {
+            min= arr[i];
+        }
+     }
+     printf("%d %d", min, max);
+     return 0;
+ }
